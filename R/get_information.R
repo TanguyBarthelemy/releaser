6b3f2d99180553019232cf1152ab7ee8a6571d02
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2025-04-10 17:07:26 +0200

    update functions

--- R/get_information.R
+++ R/get_information.R
@@ -1,5 +1,7 @@
 
 #' @export
+#' @importFrom desc description
+#'
 get_different_future_version <- function(path) {
 
     all_versions <- NULL
@@ -15,3 +17,42 @@ get_different_future_version <- function(path) {
 
     return(all_versions)
 }
+
+#' @importFrom gh gh
+#' @importFrom base64enc base64decode
+get_version_from_branch <- function(gh_repo, branch) {
+    description <- gh::gh(paste0("/repos/", gh_repo, "/contents/DESCRIPTION"),
+                          ref = branch)
+    content <- rawToChar(base64enc::base64decode(description$content))
+    nb_version <- read.dcf(textConnection(content))[, "Version"]
+    return(nb_version)
+}
+
+#' @importFrom gh gh
+#' @export
+get_latest_version <- function(gh_repo = "rjdverse/rjd3toolkit", verbose = TRUE) {
+
+    # Version sur main
+    release <- gh::gh(paste0("/repos/", gh_repo, "/releases/latest"))
+    version_main <- get_version_from_branch(gh_repo, release$tag_name)
+
+    # Version sur main
+    version_release <- get_version_from_branch(gh_repo, "main")
+
+    # Version sur develop
+    version_develop <- get_version_from_branch(gh_repo, "develop")
+
+    # Summary
+    if (verbose) {
+        cat("Derni\u00e8re release :", version_release, "\n")
+        cat("Version sur main :", version_main, "\n")
+        cat("Version sur develop :", version_develop, "\n")
+    }
+
+    if (version_release != version_main) {
+        stop("main branch of ", gh_repo, " doesn't have the same version as release.")
+    }
+
+    return(version_release)
+}
+
--- R/update_description.R
+++ R/update_description.R
@@ -1,15 +1,19 @@
 #' @export
+#' @importFrom desc desc_get_remotes desc_set_remotes
 change_remotes_field <- function(path = ".", verbose = TRUE, target = c("develop", "snapshot", "main")) {
     remotes <- desc::desc_get_remotes(path)
     if (length(remotes) == 0) return(NULL)
 
     basic_remotes <- remotes |> strsplit("@") |> sapply(`[`, 1L)
 
-    new_remotes <- switch(
-        EXPR = target,
-        develop = basic_remotes,
-        main = paste0(basic_remotes, "@*release"),
-        snapshot = paste0(basic_remotes, "@snapshot")
+    new_remotes <-  paste0(
+        basic_remotes, "@",
+        switch(
+            EXPR = target,
+            develop = "develop",
+            main ="*release",
+            snapshot = "snapshot"
+        )
     )
 
     if (verbose) {
@@ -21,6 +25,25 @@ change_remotes_field <- function(path = ".", verbose = TRUE, target = c("develop
     }
     desc::desc_set_remotes(remotes = new_remotes, file = path)
     return(invisible(new_remotes))
+}
+
+#' @export
+#' @importFrom desc desc_get_deps desc_set_dep
+set_latest_deps_version <- function(path = ".", verbose = TRUE) {
+    cur_deps <- desc::desc_get_deps(path)
+    row_rjdverse <- grep(cur_deps$package, pattern = "^rjd3")
+    for (idx in row_rjdverse) {
+        pkg <- cur_deps$package[idx]
+        pkg_type <- cur_deps$type[idx]
+        latest_version <- get_latest_version(gh_repo = paste0("rjdverse/", pkg))
+        desc::desc_set_dep(
+            package = pkg,
+            version = paste(">=", latest_version),
+            type = pkg_type,
+            file = file.path(path, "DESCRIPTION"),
+            normalize = TRUE
+        )
+    }
 }
 
 #' @export
@@ -40,7 +63,7 @@ update_news_md <- function(new_version, file, pkg) {
     old_compare_HEAD <- changelog[line_footer]
     pattern <- "v[0-9]+\\.[0-9]+\\.[0-9]+"
 
-    # Utiliser regmatches et gregexpr pour extraire le numéro de version
+    # Get version number
     matches <- regmatches(old_compare_HEAD, gregexpr(pattern, old_compare_HEAD))[[1]]
     old_version <- matches[1]
 
